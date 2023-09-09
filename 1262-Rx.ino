0ea669020aa34aab8add01583e4d7ae7e2dc718d
Hiral Shah <94632542+RHS-HIRAL@users.noreply.github.com>
2023-09-09 23:49:42 +0530

    Create 1262-Rx.ino

---
+++ 1262-Rx.ino
@@ -0,0 +1,25 @@
+#include <LoraSx1262.h>
+
+LoraSx1262 radio;
+byte receiveBuff[255];
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Booted");
+
+  if (!radio.begin()) { //Initialize the radio
+    Serial.println("Failed to initialize radio");
+  }
+}
+
+void loop() {
+  //Receive a packet over radio
+  int bytesRead = radio.lora_receive_async(receiveBuff, sizeof(receiveBuff));
+
+  if (bytesRead > -1) {
+    //Print the payload out over serial
+    Serial.print("Received: ");
+    Serial.write(receiveBuff,bytesRead);
+    Serial.println(); //Add a newline after printing
+  }
+}
